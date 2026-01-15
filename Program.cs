35b9c117f89654e9edcdc441af719b02d4e353e8
ALEKSANDR.I <107239203+AlexRMB1455AL@users.noreply.github.com>
2026-01-15 21:19:09 +0400

    srart

---
+++ Program.cs
@@ -0,0 +1,186 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
+
+/////////////////////////////////////////////////
+/// 
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
+}
+
+/*string[] fraudulentOrderIDs = new string[5];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+fraudulentOrderIDs[3] = "D000";
+fraudulentOrderIDs[4] = "E111";
+Console.WriteLine(fraudulentOrderIDs[4]); */
+
+// string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+// string[] names = { "Rowena", "Robin", "Bao" };
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// int[] inventory = { 200, 450, 700, 175, 250 };
+// int sum = 0;
+// int bin = 0;
+// foreach (int items in inventory)
+// {
+//     sum += items;
+//     bin++;
+//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
+// Console.WriteLine($"We have {sum} items in inventory.");
+
+string[] chisla = {"B123",
+"C234",
+"A345",
+"C15",
+"B177",
+"G3003",
+"C235",
+"B179"};
+
+foreach (string name in chisla)
+if (name.StartsWith("B"))
+{
+    Console.WriteLine($"The name starts with 'B'! {name}");
+}
+string firstName = "Bob";
+int widgetsPurchased = 7;
+// Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+Random randoms = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = randoms.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
+}
+// Print out each orderID
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+Random dices = new Random();
+int roll11 = dices.Next(1, 7);
+int roll22 = dices.Next(1, 7);
+int roll33 = dices.Next(1, 7);
+int total1 = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll11} + {roll22} + {roll33} = {total1}");
+if ((roll11 == roll22) || (roll22 == roll33) || (roll11 == roll33)) {
+    if ((roll11 == roll22) && (roll22 == roll33)) {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total1 += 6; 
+    } else {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total1 += 2;
+    }
+}
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) 
+{ 
+    if (i == 'o') 
+        { 
+            x++; 
+        } 
+}
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
